439cb4dc2dca1ad6d7b2c4c0cde4108ba0995343
Domenico Cascone <dcascone84@gmail.com>
2020-03-17 14:53:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,13 @@
 class Shoe
   attr_accessor :color, :size, :condition, :material
-  attr_reader :brand, :cobble
+  attr_reader :brand, :
 
   def initialize(brand)
     @brand = brand
+  end
+
+  def cobble
+    pute "Your shoe is as good as new!"
   end
 
 end
